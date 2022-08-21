6360a75bb3cd5bfca527399dc48aca2811ee8492
frendyguo <frendyguo@gmail.com>
2022-08-21 21:04:12 +0700

    feat: empty cart page

---
+++ pages/4_2_empty_cart/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $('.emptycart__recommendation .bookslider__list').slick({
+    dots: false,
+    arrows: true,
+    prevArrow: '<button class="bookslider__arrow bookslider__arrow_prev"></button>',
+    nextArrow: '<button class="bookslider__arrow bookslider__arrow_next"></button>',
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: false,
+        }
+      }
+    ]
+  });
+});
